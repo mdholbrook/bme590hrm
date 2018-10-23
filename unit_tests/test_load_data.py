7faa3a443580b2299498cd06390f0940beb477bc
Matt Holbrook <mdh54@duke.edu>
2018-10-23 18:28:51 -0400

    Added unit tests for reading csv files

---
+++ unit_tests/test_load_data.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pytest
+from functions.load_data import read_csv, clean_data
+
+
+def test_read_csv():
+    # Set up correct array
+    correct_array = np.array([[0, 2.1], [0.1, 0.1], [0.2,-0.9], [0.3, 0]])
+
+    # Run read_csv function
+    data = read_csv('data/read_test.csv')
+    print(data)
+    assert (data == correct_array).all()
+
+
+def clean_data(data):
+
+    cleaned_df = np.zeros(1)
+
+    return cleaned_df
