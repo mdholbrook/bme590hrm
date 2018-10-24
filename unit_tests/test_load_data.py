b74afde27aa1e9861abd1a548fdab17801ebe562
Matt Holbrook <mdh54@duke.edu>
2018-10-23 22:01:43 -0400

    Removed unit tests for functions not yet written

--- unit_tests/test_load_data.py
+++ unit_tests/test_load_data.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 from functions.load_data import read_csv, verify_csv_extension, clean_data
 
+
 @pytest.mark.parametrize("candidate, expected", [
     ("data/csv_test.csv", np.array([[0, 2.1], [0.1, 0.1], [0.2, -0.9], [0.3, 0]])),
     ("data/string.csv", np.array([[(2.2, np.nan), (np.nan, 3.1)]])),
@@ -13,7 +14,6 @@ def test_read_csv(candidate, expected):
 
     # Compare functions
     if np.isnan(expected).any():  # if there are strings or missing data
-        print('Is nan')
         data = data.reshape(-1)
         expected = expected.reshape(-1)
         temp = (data == expected) | (np.isnan(data) == np.isnan(expected))
@@ -37,8 +37,8 @@ def test_verify_csv_extension(candidate, expected):
     assert result == expected
 
 
-def clean_data(data):
-
-    cleaned_df = np.zeros(1)
-
-    return cleaned_df
+# def test_clean_data(data):
+#
+#     cleaned_df = np.zeros(1)
+#
+#     return cleaned_df
