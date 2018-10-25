774dafe8e76829fad7db5e7e3b8157be9683908e
Matt Holbrook <mdh54@duke.edu>
2018-10-25 00:07:16 -0400

    Created an R-peak detection function. Also added ECG normalization.
    An R-peak detection function was created based on correlation with an
    example QRS complex waveform. The resulting signal is thresholded (this
    works because both signals are normalized to 1) and non-maximum
    suppression is used to detect the peaks.

--- functions/process_ecg.py
+++ functions/process_ecg.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
+import pickle as p
 
 
 def filter_ecg(data):
@@ -27,7 +28,7 @@ def filter_ecg(data):
     # Replace the old voltage information in the numpy array
     data[:, 1] = volt_filt
 
-    return volt_filt
+    return data
 
 
 def low_pass_filter(volts, sigma=2, kernel_size=7):
@@ -83,21 +84,118 @@ def normalize_ecg(data):
     brought near 1.
 
     Args:
-        data (2D numpy array):
+        data (2D numpy array): contains two columns with time and ECG data
 
     Returns:
 
     """
 
+    # Get run duration
+    df = {}
+    df = calc_duration(data, df)
+    duration = df['duration']
+
+    # Get the number of beats to use for normalization measurement
+    num_beats_per_second = 1.0
+    beats = int(num_beats_per_second * duration)
+
+    # Find max values for each beat
+    volts = data[:, 1]
+    max_inds = np.argpartition(volts, -beats)[-beats:]
+    max_values = volts[max_inds]
+
+    # Get median peak value and normalize
+    norm_val = np.median(max_values)
+
+    # Normalize ECG
+    volts_norm = volts / norm_val
+
+    # Recreate data
+    data[:, 1] = volts_norm
+
     return data
 
 
-def r_peak_detection(df):
+def r_peak_detection(data):
+    """Detection of R-peaks in an ECG signal
 
-    inds = np.zeros(1)
-    df['r_peak_inds'] = inds
+    This function loads a sample QRS complex and computes a corrlation on an
+    input ECG signal. Areas of high correction are found via thresholding and
+    nonmaximum suppression is used to detect maxima (R-peak locations).
 
-    return df
+    Args:
+        data (2D numpy array): contains two columns with time and ECG data
+
+    Returns:
+
+    """
+
+    # Get ECG signal
+    volts = data[:, 1]
+
+    # Load sample QRS structure
+    rpeak = load_rpeak()
+
+    # Correlate the QRS cycle with the ECG
+    cor = signal.correlate(volts, rpeak, mode='same')
+    cor = np.roll(cor, 2)
+
+    # Normalize cor by the max correlation
+    cor /= np.abs(rpeak).sum()
+
+    # Threshold the correlation
+    threshold = 0.35
+    tcor = np.zeros_like(cor)
+    tcor[cor > threshold] = cor[cor > threshold]
+
+    # Non-maximum suppression for peak detection
+    max_locs = nonmax_supression(tcor)
+
+    return max_locs
+
+
+def nonmax_supression(x):
+    """Nonmaximum suppression finds crests of a signal.
+    All other non-maxima found from thresholding are suppressed.
+
+    Args:
+        x (1D numpy array): a thresholded signal
+
+    Returns:
+        max_locs (1D numpy array): an array of maxima indexes
+    """
+
+    # Get a list of indices which can shift the vector
+    inds = np.arange(1, len(x)-1, 1)
+
+    # Compare to the right and left of each point
+    compareL = (x[1:-1] - x[inds-1]) > 0
+    compareR = (x[1:-1] - x[inds+1]) > 0
+    compare = compareL * compareR
+
+    # Compute max indexes
+    max_inds = np.zeros(len(x), dtype=bool)
+    max_inds[1:-1] = compare
+
+    # Convert max_inds to a list of locations
+    max_locs = np.where(max_inds)[0]
+
+    return max_locs
+
+
+def load_rpeak():
+    """Loads a sample R-peak for beat detection
+
+    Returns:
+        rpeak (1D numpy array): sample waveform, normalized to 1
+    """
+
+    # Load sample R-peak
+    f = open('functions/rpeak.p', 'rb')
+    rpeak = p.load(f)
+    f.close()
+
+    return rpeak
 
 
 def calculate_metrics(df):
@@ -136,9 +234,13 @@ def calc_voltage_extremes(df, metrics):
     return metrics
 
 
-def calc_duration(df, metrics):
+def calc_duration(data, metrics):
 
-    metrics['duration'] = np.zeros(1)
+    # Get test duration
+    time = data[:, 0]
+    duration = time[-1] - time[0]
+
+    metrics['duration'] = duration
 
     return metrics
 
