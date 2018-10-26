49d1f0e41d4bb49d9a986d02cd6824f6e58707ac
Matt Holbrook <mdh54@duke.edu>
2018-10-26 09:00:16 -0400

    Added a function to verify the existence of the output data folder. It creates the folder if it does not exist.

--- functions/write_results.py
+++ functions/write_results.py
@@ -2,10 +2,15 @@ import os
 import json
 
 
-def write_json(df, file_descriptor):
+def write_json(df, filename):
 
-    filename = 'output_data/metrics_%s.json' % file_descriptor
+    # Generate save_path
+    gen_outpath(save_path='output_data')
 
+    # Generate a save filename based on the csv file
+    filename = gen_save_filename(filename)
+
+    # Write calculated metrics to a JSON file
     with open(filename, 'w') as fp:
         json.dump(df, fp)
 
@@ -40,3 +45,19 @@ def gen_save_filename(filename):
     new_filename = 'output_data/' + name + '.json'
 
     return new_filename
+
+
+def gen_outpath(save_path):
+    """Determines the existence of a folder and creates if if needed.
+
+    Args:
+        save_path (str): relative path to a folder in which to save the output.
+
+    Returns:
+
+    """
+
+    save_path = os.path.abspath(save_path)
+
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
