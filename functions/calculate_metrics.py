Corrected the formula for calculating the average beats per minutes. Also corrected the way that the ECG duration is assigned when there is no input or the input is out of range. This closes #19

functions/calculate_metrics.py
@@ -15,7 +15,7 @@ def calculate_metrics(data, data_filt, rpeak_locs, duration):
         check_input_duration(duration, metrics)
     except ValueError:
         print('Proceeding using the entire ECG range.')
-        duration = (0, metrics['duration'])
+        duration = (0, metrics['duration']/60)
 
     # Calculate voltage extremes
     metrics = calc_voltage_extremes(data, metrics)
@@ -86,7 +86,7 @@ def calc_mean_hr_bpm(duration, metrics):
 
     # Get list of times for each beat, in seconds, during the interval
     beat_times = beats[start_ind*end_ind]
-    bpm = np.mean(beat_times[1:] - beat_times[:-1]) * 60  # [beats per min]
+    bpm = 60 / np.mean(beat_times[1:] - beat_times[:-1])  # [beats per min]
 
     # Assign bpm to the dictionary
     metrics['mean_hr_bpm'] = bpm

unit_tests/test_calculate_metrics.py
@@ -80,7 +80,7 @@ def test_calc_mean_hr_bpm():
     beats_per_second = 1.2
     metrics = {}
     metrics['beats'] = np.arange(0, 180, beats_per_second)  # [sec]
-    bpm = beats_per_second * 60
+    bpm = 60 / beats_per_second
 
     # Call the function
     metrics = calc_mean_hr_bpm(duration, metrics)