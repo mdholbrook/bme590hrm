4490f91ed245445ea67ae170fce6aea592004755
Matt Holbrook <mdh54@duke.edu>
2018-10-25 21:40:21 -0400

    Added unit test for calc_beats

--- unit_tests/test_calculate_metrics.py
+++ unit_tests/test_calculate_metrics.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from functions.calculate_metrics import calc_num_beats
+from functions.calculate_metrics import calc_num_beats, calc_beats
 
 
 def test_calc_num_beats():
@@ -18,3 +18,21 @@ def test_calc_num_beats():
     metrics = calc_num_beats(rpeak_locs, metrics)
 
     assert metrics['num_beats'] == num_beats
+
+
+def test_calc_beats():
+
+    # Set up test data
+    time = np.arange(0, 10, 0.1)
+    locs = np.random.choice(100, 10, replace=False)
+    times = time[locs]
+
+    # Set up data array and metrics dictionary
+    data = np.zeros((100, 2))
+    data[:, 0] = time
+    metrics = {}
+
+    # Run the function
+    metrics = calc_beats(data, rpeak_locs=locs, metrics=metrics)
+
+    assert (metrics['beats'] == times).all()
